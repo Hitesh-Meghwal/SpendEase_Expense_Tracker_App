1296d76b55dce1275a6552df60c07e01c3f0d56a
Hitesh Meghwal <129782600+Hitesh-Meghwal@users.noreply.github.com>
2023-05-05 01:00:41 +0530

    Log in UI done

--- app/src/main/java/com/example/spendease/MainActivity.kt
+++ app/src/main/java/com/example/spendease/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.spendease
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.spendease.UserAuthentication.Login
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button_id)
+        btn.setOnClickListener {
+            val i = Intent(this,Login::class.java)
+            startActivity(i)
+        }
     }
 }
